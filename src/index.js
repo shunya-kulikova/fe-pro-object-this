df75d90bfb2ed4b08f64659278138df4618c5ce5
unknown <a.kulikova.design@gmail.com>
2022-08-02 21:26:09 +0300

    1st

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-export const hotel = new Object();
+export const hotel = {
+    quantityOfPlaces: 30,
+    priceByPlace: 20,
+    bankAccount: 0,
+    guests: {},
+  
+    getLength() {
+      return Object.keys(this.guests).length;
+    },
+  
+    getActualFreePlace() {
+      return this.quantityOfPlaces - this.getLength();
+    },
+  
+    paidPerPlace() {
+      return (this.bankAccount += this.priceByPlace);
+    },
+  
+    checkInGuest(guestFirstName, guestLastName, guestMoney) {
+      if (!this.getActualFreePlace() > 0) {
+        return 'Sorry, we have not free spaces';
+      }
+  
+      if (guestMoney < this.priceByPlace) {
+        return 'Sorry, you have not enough money';
+      }
+  
+      this.paidPerPlace();
+  
+      return (this.guests[this.getLength()] = {
+        firstName: guestFirstName,
+        lastName: guestLastName,
+        money: (guestMoney -= this.priceByPlace),
+      });
+    },
+  };
